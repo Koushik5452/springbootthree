0c68ebc36008712056b1864cca22aab90f0f6ae7
Koushik5452 <annemkoushikreddy5452@gmail.com>
2024-12-29 17:47:20 +0530

    thirdone

---
+++ src/main/java/com/example/springbootthree/SpringbootthreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootthreeApplication.class, args);
+	}
+
+}
